489fd55e3c3d6527932579f8b203f141fd6e8fee
Cristian Gomez <1154@holbertonschool.com>
2020-03-10 20:00:11 +0000

    Adding task 7, update main folder

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_inorder - function that usign in-order traversal.
+* @tree: node to check
+* @func: pointer to a function
+* Return: none
+*/
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree)
+		return;
+
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+}

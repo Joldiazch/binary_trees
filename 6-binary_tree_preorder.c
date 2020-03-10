558088f1fe74a96255e8195c2eee0b9eeab4af1d
joldiazch <jluis.diaz@udea.edu.co>
2020-03-10 20:07:24 +0000

    make binary_tree_preorder function

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_preorder - function that usign pre-order traversal.
+* @tree: node to check
+* @func: pointer to a function
+* Return: none
+*/
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree)
+		return;
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}

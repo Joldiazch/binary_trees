92cfcd955ed21592a01cbd7b910add4f751dd960
Cristian Gomez <1154@holbertonschool.com>
2020-03-10 16:14:09 +0000

    Adding condition in malloc

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,7 +9,11 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
 
 	new->parent = parent;
 	new->n = value;

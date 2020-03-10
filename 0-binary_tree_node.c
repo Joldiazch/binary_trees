c942a840f9e74afe93b672629d8ad66948e2038a
Jose <jluis.diaz@udea.edu.co>
2020-03-10 15:06:43 +0000

    files task0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new = NULL;
+
+    new = malloc(sizeof(binary_tree_t));
+    if(!new)
+        return NULL;
+    if (!parent)
+    {
+
+    }  
+}

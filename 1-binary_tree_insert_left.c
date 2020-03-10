73041163098f2eeb7ab3619ede9d585f4d39e093
joldiazch <jluis.diaz@udea.edu.co>
2020-03-10 17:15:46 +0000

    add binary_tree_insert_left function into 1-binary_tree_insert_lef

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_node - function that creates a binary tree node.
+* binary_tree_insert_left - function that create an child-left node.
 * @parent: node parent
 * @value: the value
 * Return: new node
@@ -9,5 +9,29 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	
+	int aux;
+
+	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+
+	if (!new || !parent)
+		return (NULL);
+
+	new->left = NULL;
+	new->right = NULL;
+
+	if (parent->left)
+	{
+		aux = parent->left->n;
+		parent->left->n = value;
+		new->n = aux;
+		new->parent = parent->left;
+		parent->left->left = new;
+	}
+	else
+	{
+		new->n = value;
+		new->parent = parent;
+		parent->left = new;
+	}
+	return (new);
 }

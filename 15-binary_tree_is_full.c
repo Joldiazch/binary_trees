670c774c9fa39d9a82cd957d22835364da1898df
Cristian Gomez <1154@holbertonschool.com>
2020-03-11 23:58:25 +0000

    Adding task 15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,42 @@
+#include "binary_trees.h"
+
+/**
+* count - function that measures the height of a binary tree.
+* @tree: node parent
+* Return: none
+*/
+
+int count(const binary_tree_t *tree)
+{
+	int l_node, r_node;
+
+	if (!tree)
+		return (0);
+
+	l_node = tree->left ? count(tree->left) : 0;
+	r_node = tree->right ? count(tree->right) : 0;
+
+	return (l_node + r_node + 1);
+}
+
+/**
+* binary_tree_is_full - function that measures the height of a binary tree.
+* @tree: node parent
+* Return: none
+*/
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int l_node, r_node;
+
+	if (!tree)
+		return (0);
+
+	l_node = tree->left ? count(tree->left) : 0;
+	r_node = tree->right ? count(tree->right) : 0;
+
+	if (l_node == r_node)
+		return (1);
+	else
+		return (0);
+}

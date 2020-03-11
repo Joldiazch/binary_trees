38d78728683b6e04e2fedb07a58b73133df0bad2
Cristian Gomez <1154@holbertonschool.com>
2020-03-11 21:26:46 +0000

    Updating task 1 and 2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!parent)
 		return (NULL);
 
-	*new = malloc(sizeof(binary_tree_t));
+	new = malloc(sizeof(binary_tree_t));
 	if (!new)
 		return (NULL);
 

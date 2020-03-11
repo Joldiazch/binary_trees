d7177a51497a5b89e41e9d42e0ae01418ede5720
Cristian Gomez <1154@holbertonschool.com>
2020-03-11 22:13:28 +0000

    Updating task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,23 @@
 #include "binary_trees.h"
 
 /**
+* height - function that measures the height of a binary tree.
+* @tree: node parent
+* Return: none
+*/
+
+int height(const binary_tree_t *tree)
+{
+	int h_left, h_right;
+
+	if (!tree)
+		return (0);
+	h_left = tree->left ? 1 + height(tree->left) : 0;
+	h_right = tree->right ? 1 + height(tree->right) : 0;
+	return (h_left > h_right ? h_left : h_right);
+}
+
+/**
 * binary_tree_balance - function that measures the balance of a binary tree
 * @tree: root node.
 * Return: Balance nodes.
@@ -12,7 +29,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	h_left = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
-	h_right = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
+	h_left = tree->left ? 1 + height(tree->left) : 0;
+	h_right = tree->right ? 1 + height(tree->right) : 0;
 	return (h_left - h_right);
 }

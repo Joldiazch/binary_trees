b7edf3f9242c533ef07c4eb3ed68673fb96d279b
cristian gomez <1154@holbertonschool.com>
2020-03-12 21:54:51 +0000

    Updating task 100

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,72 +1,6 @@
 #include "binary_trees.h"
 
 /**
-* uncle - function that find uncles.
-* @node: node parent
-* Return: node uncle
-*/
-
-binary_tree_t *uncle(const binary_tree_t *node)
-{
-	int value;
-
-	if (!node)
-		return (NULL);
-
-	if (node->parent)
-	{
-		value = node->parent->n;
-
-		if (node->parent->parent)
-		{
-			if (node->parent->parent->left && node->parent->parent->right)
-			{
-				if (node->parent->parent->left && node->parent->parent->left->n != value)
-					return (node->parent->parent->left);
-
-				else if (node->parent->parent->right &&
-						node->parent->parent->right->n != value)
-					return (node->parent->parent->right);
-
-				else
-					return (NULL);
-			}
-		}
-		return (NULL);
-	}
-	return (NULL);
-}
-#include "binary_trees.h"
-
-/**
-* sibling - function that find siblings.
-* @node: node parent
-* Return: node sibling
-*/
-
-binary_tree_t *sibling(const binary_tree_t *node)
-{
-	int value;
-
-	if (!node || !node->parent)
-		return (NULL);
-
-	value = node->n;
-
-	if (node->parent->left && node->parent->right)
-	{
-		if (node->parent->left->n != value &&
-			node->parent->left)
-			return (node->parent->left);
-
-		else if (node->parent->right->n != value &&
-				node->parent->right)
-			return (node->parent->right);
-	}
-	return (NULL);
-}
-
-/**
 * binary_trees_ancestor - function that find siblings.
 * @first: node parent
 * @second: node
@@ -76,17 +10,22 @@ binary_tree_t *sibling(const binary_tree_t *node)
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 const binary_tree_t *second)
 {
+	binary_tree_t *l, *r;
+
 	if (!first || !second)
 		return (NULL);
-	if (first->n == second->n)
+
+	if (first == second)
 		return ((binary_tree_t *)first);
-	if (first->parent == second)
-		return ((binary_tree_t *)second);
-	if (second->parent == first)
-		return ((binary_tree_t *)first);
-	if (sibling(first) == second || uncle(second) == first)
-		return (first->parent);
-	if (uncle(first) == second)
-		return (second->parent);
-	return (binary_trees_ancestor(first->parent, second->parent));
+
+	l = first->parent;
+	r = second->parent;
+
+	if (first == r || !l || (!l->parent && r))
+		return (binary_trees_ancestor(first, r));
+
+	else if (l == second || !r || (!r->parent && l))
+		return (binary_trees_ancestor(l, second));
+
+	return (binary_trees_ancestor(l, r));
 }
